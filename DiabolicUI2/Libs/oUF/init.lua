348e8f06dd3594d6606d7423e95253db7141139a
Lars Norberg <github@larsnorbergofficial.com>
2022-10-31 14:10:54 +0100

    update client constants

--- DiabolicUI2/Libs/oUF/init.lua
+++ DiabolicUI2/Libs/oUF/init.lua
@@ -2,19 +2,8 @@ local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
 
-local patch, build, date, version = GetBuildInfo()
-local major, minor = string.split(".", patch)
-
-ns.oUF.clientVersion = version
-ns.oUF.clientDate = date
-ns.oUF.clientPatch = patch
-ns.oUF.clientMajor = tonumber(major)
-ns.oUF.clientMinor = tonumber(minor)
-ns.oUF.clientBuild = tonumber(build)
-
 ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 ns.oUF.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 ns.oUF.isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
-
-ns.oUF.WoW10 = version >= 100000
+ns.oUF.WoW10 = select(4, GetBuildInfo()) >= 100000
