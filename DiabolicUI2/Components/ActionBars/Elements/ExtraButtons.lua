4400401efb6dd8c04c879f35e63d87405cda7c92
Lars Norberg <github@larsnorbergofficial.com>
2022-12-08 09:11:51 +0100

    fix extrabuttons for 10.0.2

--- DiabolicUI2/Components/ActionBars/Elements/ExtraButtons.lua
+++ DiabolicUI2/Components/ActionBars/Elements/ExtraButtons.lua
@@ -226,9 +226,10 @@ ExtraButtons.OnInitialize = function(self)
 		if (UIPARENT_MANAGED_FRAME_POSITIONS) then
 			UIPARENT_MANAGED_FRAME_POSITIONS.ExtraAbilityContainer = nil
 		end
-		ExtraAbilityContainer.SetSize = noop
+		--ExtraAbilityContainer.SetSize = noop -- taints the editmode
 		ExtraAbilityContainer:SetFrameStrata("LOW")
 		ExtraAbilityContainer:SetFrameLevel(10)
+		ExtraAbilityContainer.ignoreFramePositionManager = true
 		ExtraActionBarFrame:SetParent(extraScaffold)
 		ExtraActionBarFrame:ClearAllPoints()
 		ExtraActionBarFrame:SetAllPoints()
